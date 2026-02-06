aece98428209356b3b0ab023931c9cae5a0951b2
RamyaPativada <ramyapativada@gmail.com>
2026-02-06 11:16:19 +0530

    set time out to zero

--- blocking.js
+++ blocking.js
@@ -5,6 +5,11 @@ console.log("hello world");
 
 crypto.pbkdf2Sync("password","salt",500000,50,"sha512");
 console.log("the 1st key is generated");
+
+setTimeout(()=>{
+    console.log("call me asap");
+
+},0);
 crypto.pbkdf2("ramya","salt",500000,50,"sha512",(err,key)=>{
     console.log("the 2nd key is generated");
 });
